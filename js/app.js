183622418ff237f9e3bca6715c2387335026dd3a
Eugen Neufeld <eneufeld@eclipsesource.com>
2016-02-05 17:01:39 +0100

    refactored to not be a spa

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 angular.module('jsonforms-website', [
     'ngMaterial',
-    'ngRoute',
+    //'ngRoute',
     'ui.ace',
     //'ui.bootstrap',
     //'ui.validate',
@@ -63,6 +63,7 @@ angular.module('jsonforms-website', [
   $mdThemingProvider.theme('default')
     .primaryPalette('esPaletteDark')
 })
+/*
 .config(
         function($routeProvider, $locationProvider) {
             $routeProvider.when('/home', {
@@ -101,27 +102,63 @@ angular.module('jsonforms-website', [
             $locationProvider.html5Mode(true);
         }
     )
+*/
 ;
 
 var app = angular.module('jsonforms-website');
-app.controller('AppCtrl', function()  {
+app.controller('AppCtrl',['StaticData','DynamicData', function(StaticData,DynamicData)  {
     var vm = this;
+    /*
     vm.showMainNav = false;
     vm.toggleMainMenu = function () {
         vm.showMainNav = !vm.showMainNav;
     };
-});
-app.controller('SupportCtrl', function()  {});
-app.controller('DocsCtrl',['$scope', function($scope)  {
-    $scope.showDocsNav = false;
-    $scope.toggleDocsMenu = function () {
-        $scope.showDocsNav = !$scope.showDocsNav;
+*/
+    vm.showDocsNav = false;
+    vm.toggleDocsMenu = function () {
+        vm.showDocsNav = !vm.showDocsNav;
+    };
+    vm.isSelected=function(selected) {
+        //return selected==$location.path();
+        return false;
+    }
+
+    vm.staticDataProvider=StaticData;
+    vm.dynamicDataProvider=DynamicData;
+    vm.aceLoaded = function(editor) {
+        editor.$blockScrolling = Infinity;
+        editor.getSession().setMode("ace/mode/javascript");
+        editor.setOptions({
+            enableSnippets: true,
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true
+        });
+    };
+
+    vm.localStaticModelObject=vm.staticDataProvider.dataSchema;
+    vm.localStaticViewObject=vm.staticDataProvider.uiSchema;
+    vm.localStaticModel = JSON.stringify(vm.localStaticModelObject, undefined, 2);
+    vm.localStaticView = JSON.stringify(vm.localStaticViewObject, undefined, 2);
+
+    vm.reparseStatic = function() {
+        vm.localStaticModelObject = JSON.parse(vm.localStaticModel);
+        vm.localStaticViewObject = JSON.parse(vm.localStaticView);
+    };
+
+    vm.localDynamicModelObject=vm.dynamicDataProvider.dataSchema;
+    vm.localDynamicViewObject=vm.dynamicDataProvider.uiSchema;
+    vm.localDynamicModel = JSON.stringify(vm.localDynamicModelObject, undefined, 2);
+    vm.localDynamicView = JSON.stringify(vm.localDynamicViewObject, undefined, 2);
+
+    vm.reparseDynamic = function() {
+        vm.localDynamicModelObject = JSON.parse(vm.localDynamicModel);
+        vm.localDynamicViewObject = JSON.parse(vm.localDynamicView);
     };
 }]);
+
 app.directive('docsMenu', ['$location',function($location) {
   return {
     restrict: 'E',
-    scope:{},
     templateUrl: 'docs/docs_menu.html',
     link: function (scope) {
       scope.showDocsNav = false;
@@ -129,8 +166,8 @@ app.directive('docsMenu', ['$location',function($location) {
           scope.showDocsNav = !scope.showDocsNav;
       };
       scope.isSelected=function(selected) {
-          return selected==$location.path();
-      }
+          return $location.absUrl().endsWith(selected);
+      };
     }
   };
 }]);
@@ -145,4 +182,189 @@ app.directive('mainMenu', function() {
         };
     }
   };
+});
+
+app.factory('StaticData', function() {
+    var provider={};
+    provider.data={};
+    provider.dataSchema={
+        "type": "object",
+        "properties": {
+            "firstName": {
+                "type": "string"
+            },
+            "lastName": {
+                "type": "string"
+            }
+        }
+    };
+    provider.uiSchema={
+        "type": "VerticalLayout",
+        "elements": [
+            {
+                "type": "Control",
+                "scope": {
+                    "$ref": "#/properties/firstName"
+                }
+            },
+            {
+                "type": "Control",
+                "scope": {
+                    "$ref": "#/properties/lastName"
+                }
+            }
+        ]
+    };
+    return provider;
+});
+app.factory('DynamicData', function() {
+    var provider={};
+    provider.data={};
+    provider.dataSchema={
+        "type": "object",
+        "properties": {
+            "id": {
+                "type": "string",
+                "format": "objectId"
+            },
+            "lastName": {
+                "type": "string"
+            },
+            "email": {
+                "type": "string"
+            },
+            "firstName": {
+                "type": "string"
+            },
+            "gender": {
+                "type": "string",
+                "enum": [
+                    "Male",
+                    "Female"
+                ]
+            },
+            "active": {
+                "type": "boolean"
+            },
+            "timeOfRegistration": {
+                "type": "string",
+                "format": "date-time"
+            },
+            "weight": {
+                "type": "number"
+            },
+            "height": {
+                "type": "integer"
+            },
+            "nationality": {
+                "type": "string",
+                "enum": [
+                    "German",
+                    "French",
+                    "UK",
+                    "US",
+                    "Spanish",
+                    "Italian",
+                    "Russian"
+                ]
+            },
+            "dateOfBirth": {
+                "type": "string",
+                "format": "date-time"
+            }
+        },
+        "additionalProperties": false,
+        "required": [
+            "id",
+            "lastName",
+            "email"
+        ]
+    };
+    provider.uiSchema={
+        "type": "VerticalLayout",
+        "elements": [
+            {
+                "type": "HorizontalLayout",
+                "elements": [
+                    {
+                        "type": "VerticalLayout",
+                        "elements": [
+                            {
+                                "type": "Control",
+                                "scope": {
+                                    "$ref": "#/properties/firstName"
+                                }
+                            },
+                            {
+                                "type": "Control",
+                                "scope": {
+                                    "$ref": "#/properties/lastName"
+                                }
+                            },
+                            {
+                                "type": "Control",
+                                "scope": {
+                                    "$ref": "#/properties/dateOfBirth"
+                                }
+                            },
+                            {
+                                "type": "HorizontalLayout",
+                                "elements": [
+                                    {
+                                        "type": "Control",
+                                        "scope": {
+                                            "$ref": "#/properties/weight"
+                                        }
+                                    },
+                                    {
+                                        "type": "Control",
+                                        "scope": {
+                                            "$ref": "#/properties/height"
+                                        }
+                                    },
+                                    {
+                                        "type": "Control",
+                                        "scope": {
+                                            "$ref": "#/properties/nationality"
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "Control",
+                                "scope": {
+                                    "$ref": "#/properties/gender"
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        "type": "VerticalLayout",
+                        "elements": [
+                            {
+                                "type": "Control",
+                                "scope": {
+                                    "$ref": "#/properties/timeOfRegistration"
+                                }
+                            },
+                            {
+                                "type": "Control",
+                                "scope": {
+                                    "$ref": "#/properties/email"
+                                }
+                            },
+                            {
+                                "type": "Control",
+                                "scope": {
+                                    "$ref": "#/properties/active"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+
+        ]
+    };
+    return provider;
 });
